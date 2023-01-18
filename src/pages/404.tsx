52681753503cc07691aad3ab70277fb423005ac3
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-18 21:33:54 +0200

    Add page 404
    Add page 404 with timer that will redirect user to main page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState, SetStateAction } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 function NotGoundPage() {
+  const [redirectTime, setRedirectTime] = useState(8)
+  const router = useRouter()
+  useEffect(() => {
+    const timer = setInterval(() => {
+        setRedirectTime((prevState) => prevState - 1)
+      }, 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  if (redirectTime <= 0){
+    router.push('/')
+  }
   return (
     <div className='not-found'>
       <h1>Oops...</h1>
       <h2>Such page does not exist!</h2>
+      <p>You will be rederected to Homepage in {redirectTime} seconds</p>
       <p>Go back to <Link href='/'>Home</Link></p>
     </div>
   )
