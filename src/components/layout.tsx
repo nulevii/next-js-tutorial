598b6950db4387ef17af8437dbedf9cfd2164843
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-18 22:23:05 +0200

    Add Head with meta data

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Head from 'next/head'
 import Header from './header'
 import Footer from './footer'
 function Layout({children}: {children: JSX.Element}) {
   return (
+    <>
+    <Head>
+    <title>Healthy Burgers</title>
+    <meta name='title' content='Healthy Burgers'/>
+    </Head>
     <div className='content'>
       <Header/>
       {children}
       <Footer/>
     </div>
+    </>
   )
 }
 
