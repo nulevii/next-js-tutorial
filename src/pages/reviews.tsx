9b3839568d51ed193af743a0a36414963c8e358d
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-19 12:28:35 +0200

    Create reviews component using JSON.Placeholder

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
-async function  Reviews ()  {
+  interface IReview {
+    body: string
+    email: string
+    id: number
+    name: string
+    postID: number
+  }
+ function  Reviews ({reviews}: {reviews: IReview[]})  {
+  const el = true
   return (
-    <div><h1>Reviews</h1></div>
+    <div>
+      <h1>Clients reviews</h1>
+      <div className="reviews">
+        {reviews.map(review => (
+          <div key={review.id} className='review'>
+            {review.id}{' '}
+            {`${review.body.slice(0, 90)}...`}
+          </div>
+        ))}
+      </div>
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  const responce = await fetch('https://jsonplaceholder.typicode.com/comments');
+  const data = await responce.json()
+  return {
+    props: {
+      reviews: data.slice(0,20)
+    }
+  }
 }
 
 export default Reviews
